9910c278c195729099850c81531c59b08b3eef8d
Song Kim <keyrow223@gmail.com>
2020-02-18 08:09:41 -0800

    Bruce?

---
+++ Bruce.js
@@ -0,0 +1,6 @@
+var name = "Bruce"; // variable set to Bruce
+if (name != "Bruce") {   // if name var not set to Bruce it will ask what your name is but it is Bruce 
+    console.log("What's your name?");
+} else {    
+    console.log("Hello, Bruce!"); // This is what will be printed
+}
