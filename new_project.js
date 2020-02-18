407d6f16e416b7a849c8db696ab123cc7c85e0e6
Song Kim <keyrow223@gmail.com>
2020-02-18 08:01:03 -0800

    updated

--- new_project.js
+++ new_project.js
@@ -2,4 +2,5 @@ console.log("New project woohoo. In here I will:");
 console.log("1.) Create a Private GitHub Repo called first_project"); 
 console.log("2.)Navigate to your new_project folder in your terminal and link it to your remote repo");
 console.log("3.)Push your local git repo to your online remote repo"); 
-console.log("4.)Write your commands in a text file and upload it below")
+console.log("4.)Write your commands in a text file and upload it below");
+console.log("updated");
